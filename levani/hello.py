ebfef926b0cf8187a4f21dd8a13c2f78cf6a6dbc
levani danelashvili <levani.danelashvili.1@btu.edu.ge>
2022-05-28 13:16:08 +0400

    nothing

--- levani/hello.py
+++ levani/hello.py
@@ -3,10 +3,10 @@ app = Flask(__name__)
 
 @app.route('/<home>')
 def hello_world(home):
-    return 'Hello '+ home
+    return 'Hello ' + home
 
 @app.route('/')
 def hey():
-    return 'ss'
+    return 'nothing'
 if __name__ == '__main__':
     app.run(debug=True)
