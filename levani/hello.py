471ff1668f22ec955fd23f6547919365a60fa58a
levani danelashvili <levani.danelashvili.1@btu.edu.ge>
2022-05-28 13:13:16 +0400

    first commit

---
+++ levani/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<home>')
+def hello_world(home):
+    return 'Hello '+ home
+
+@app.route('/')
+def hey():
+    return 'ss'
+if __name__ == '__main__':
+    app.run(debug=True)
